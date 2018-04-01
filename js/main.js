5f147a75e23f66a870b9d78865cc0309e8dfbff4
St3ve89 <istvan.acs89@gmail.com>
2018-04-01 15:49:18 +0100

    checking how local storage is working in JavaScript

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@
 
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-function saveBookmark(){
-    console.log('it works');
+function saveBookmark(e){
+    // get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
+
+    var bookmark = {
+        name: siteName,
+        url: siteURL
+    }
+
+    // local storage test
+    localStorage.setItem('test', 'hello word');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+   
+    // prevent form from submitting
+    e.preventDefault();
 }
