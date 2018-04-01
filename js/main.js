998a78bbf0e36b14ef1429c774af5883539c15f5
St3ve89 <istvan.acs89@gmail.com>
2018-04-01 16:38:16 +0100

    add if function to test the bookmars is null, initialize array,add items to array, set to local storage ..

--- js/main.js
+++ js/main.js
@@ -12,12 +12,32 @@ function saveBookmark(e){
         url: siteURL
     }
 
-    // local storage test
-    localStorage.setItem('test', 'hello word');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
+    // // local storage test
+    // localStorage.setItem('test', 'hello word');
+    // console.log(localStorage.getItem('test'));
+    // localStorage.removeItem('test');
+    // console.log(localStorage.getItem('test'));
    
+
+    // test if bookmarks is null
+    if(localStorage.getItem('bookmarks') === null){
+        // init array
+        var bookmarks = [];
+        // add to array
+        bookmarks.push(bookmark);
+        //set to local storage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks)); 
+    } else {
+        // get bookmarks from localstorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // add bookmark to array
+        bookmarks.push(bookmark);
+        // re-set it back to local storage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks)); 
+    }
+
+
+
     // prevent form from submitting
     e.preventDefault();
 }
